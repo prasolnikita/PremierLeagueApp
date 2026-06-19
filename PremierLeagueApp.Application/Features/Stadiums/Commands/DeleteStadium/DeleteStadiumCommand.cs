077255098d7e56e29da6b55a5959abdb9e5e3a24
prasolnikita <113930790+prasolnikita@users.noreply.github.com>
2026-06-19 17:50:46 +0200

    Application: Delete Stadium

---
+++ PremierLeagueApp.Application/Features/Stadiums/Commands/DeleteStadium/DeleteStadiumCommand.cs
@@ -0,0 +1,6 @@
+﻿using LiteBus.Commands.Abstractions;
+
+namespace PremierLeagueApp.Application.Features.Stadiums.Commands.DeleteStadium
+{
+    public record DeleteStadiumCommand(Guid StadiumId) : ICommand;
+}
---
+++ PremierLeagueApp.Application/Features/Stadiums/Commands/DeleteStadium/DeleteStadiumCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using LiteBus.Commands.Abstractions;
+using PremierLeagueApp.Domain.Features.Stadiums;
+
+namespace PremierLeagueApp.Application.Features.Stadiums.Commands.DeleteStadium
+{
+    public class DeleteStadiumCommandHandler : ICommandHandler<DeleteStadiumCommand>
+    {
+        private readonly IStadiumRepository _stadiumRepository;
+
+        public DeleteStadiumCommandHandler (IStadiumRepository stadiumRepository)
+        {
+            _stadiumRepository = stadiumRepository;
+        }
+
+        public async Task HandleAsync(DeleteStadiumCommand command, CancellationToken cancellationToken = default)
+        {
+            var stadiumId = new StadiumId(command.StadiumId);
+
+            var stadium = await _stadiumRepository.GetByIdAsync(stadiumId)
+                ?? throw new KeyNotFoundException($"Stadium was not found");
+
+            stadium.MarkAsDeleted();
+
+            await _stadiumRepository.CommitAsync();
+        }
+    }
+}
